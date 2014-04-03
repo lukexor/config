ba8ac42480d28b796369bd22f4ee7622af1b42d1
Luke Petherbridge <lukexor@gmail.com>
2014-04-03 16:57:32 -0700

    Removed escape

--- themes/zhayedan.sh
+++ themes/zhayedan.sh
@@ -2,28 +2,28 @@
 # == zhayedan bash theme
 
 # GIT
-BASH_THEME_GIT_PROMPT_PREFIX=" \033${FGcyan}["
-BASH_THEME_GIT_PROMPT_SUFFIX="\033${FGcyan}]\033${RCLR}"
-BASH_THEME_GIT_PROMPT_CLEAN="\033${FGgreen} =\033${RCLR}"
+BASH_THEME_GIT_PROMPT_PREFIX=" ${FGcyan}["
+BASH_THEME_GIT_PROMPT_SUFFIX="${FGcyan}]${RCLR}"
+BASH_THEME_GIT_PROMPT_CLEAN="${FGgreen} =${RCLR}"
 
-BASH_THEME_GIT_PROMPT_AHEAD="\033$FGbred}!\033${RCLR}"
+BASH_THEME_GIT_PROMPT_AHEAD="$FGbred}!${RCLR}"
 
-BASH_THEME_GIT_PROMPT_ADDED="\033${FGgreen}+\033${RCLR}"
-BASH_THEME_GIT_PROMPT_MODIFIED="\033${FGblue}x\033${RCLR}"
-BASH_THEME_GIT_PROMPT_DELETED="\033${FGred}-\033${RCLR}"
-BASH_THEME_GIT_PROMPT_RENAMED="\033${FGmagenta}>\033${RCLR}"
-BASH_THEME_GIT_PROMPT_UNMERGED="\033${FGyellow}^\033${RCLR}"
-BASH_THEME_GIT_PROMPT_UNTRACKED="\033${FGcyan}.\033${RCLR}"
+BASH_THEME_GIT_PROMPT_ADDED="${FGgreen}+${RCLR}"
+BASH_THEME_GIT_PROMPT_MODIFIED="${FGblue}x${RCLR}"
+BASH_THEME_GIT_PROMPT_DELETED="${FGred}-${RCLR}"
+BASH_THEME_GIT_PROMPT_RENAMED="${FGmagenta}>${RCLR}"
+BASH_THEME_GIT_PROMPT_UNMERGED="${FGyellow}^${RCLR}"
+BASH_THEME_GIT_PROMPT_UNTRACKED="${FGcyan}.${RCLR}"
 
 # Format for git_prompt_long_sha() and git_prompt_short_sha()
-BASH_THEME_GIT_PROMPT_SHA_BEFORE=" \033${FGyellow}"
-BASH_THEME_GIT_PROMPT_SHA_AFTER="\033${RCLR}"
+BASH_THEME_GIT_PROMPT_SHA_BEFORE=" ${FGyellow}"
+BASH_THEME_GIT_PROMPT_SHA_AFTER="${RCLR}"
 
 # Colors vary depending on time lapsed.
-BASH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="\033${FGgreen}"
-BASH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="\033${FGyellow}"
-BASH_THEME_GIT_TIME_SINCE_COMMIT_LONG="\033${FGred}"
-BASH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="\033${FGcyan}"
+BASH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="${FGgreen}"
+BASH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="${FGyellow}"
+BASH_THEME_GIT_TIME_SINCE_COMMIT_LONG="${FGred}"
+BASH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="${FGcyan}"
 
 # Git time since commit
 BASH_THEME_GIT_TIME_SINCE_COMMIT_BEFORE=""
@@ -32,12 +32,12 @@ BASH_THEME_GIT_TIME_SINCE_COMMIT_AFTER=""
 # SVN
 PROMPT_BASE_COLOR=""
 BASH_THEME_REPO_NAME_COLOR=""
-BASH_THEME_SVN_PROMPT_PREFIX=" \033${FGcyan}["
-BASH_THEME_SVN_PROMPT_SUFFIX="\033${FGcyan}]\033${RCLR}"
-BASH_THEME_SVN_PROMPT_DIRTY="\033${FGred} *\033${RCLR}"
-BASH_THEME_SVN_PROMPT_CLEAN="\033${FGgreen} =\033${RCLR}"
-BASH_THEME_SVN_PROMPT_REV_BEFORE=" \033${FGyellow}"
-BASH_THEME_SVN_PROMPT_REV_AFTER="\033${RCLR}"
+BASH_THEME_SVN_PROMPT_PREFIX=" ${FGcyan}["
+BASH_THEME_SVN_PROMPT_SUFFIX="${FGcyan}]${RCLR}"
+BASH_THEME_SVN_PROMPT_DIRTY="${FGred} *${RCLR}"
+BASH_THEME_SVN_PROMPT_CLEAN="${FGgreen} =${RCLR}"
+BASH_THEME_SVN_PROMPT_REV_BEFORE=" ${FGyellow}"
+BASH_THEME_SVN_PROMPT_REV_AFTER="${RCLR}"
 
 # Trim working path to 1/2 of screen width
 function prompt_pwd() {
@@ -56,13 +56,13 @@ function prompt_on() {
     # User is colored differently based on root user
     if [ "${UID}" -eq 0 ]
     then
-        PR_CLR="\033${BGred}${FGwhite}"
+        PR_CLR="${BGred}${FGwhite}"
     else
         if [[ "${USER}" == "${LOGNAME}" ]]
         then
-            PR_CLR="\033${RCLR}${FGred}"
+            PR_CLR="${RCLR}${FGred}"
         else
-            PR_CLR="\033${RCLR}${FGgreen}"
+            PR_CLR="${RCLR}${FGgreen}"
         fi
     fi
     export PR_CLR
@@ -71,7 +71,7 @@ function prompt_on() {
     TTY=$(tty)
     case "${TERM}" in
         xterm* )
-            PROMPT_COMMAND='history -a; history -c; history -r; echo -ne "\033${USER}(${SHLVL}): $(prompt_pwd) | ${TTY:5}\007";'
+            PROMPT_COMMAND='history -a; history -c; history -r; echo -ne "${USER}(${SHLVL}): $(prompt_pwd) | ${TTY:5}\007";'
             ;;
         screen* )
             PROMPT_COMMAND='history -a; history -c; history -r; echo -ne "\033_screen \005 (\005t) | ${USER}(${SHLVL}): $(prompt_pwd) | ${TTY:5}\033\\";'
@@ -119,23 +119,23 @@ function prompt_on() {
     # fi
 
     # Left command prompt
-    PS1="${PR_CLR}${HOSTNAME}\033${FGgreen}"'$(prompt_pwd)$(battery_pct_prompt)'
+    PS1="${PR_CLR}${HOSTNAME}${FGgreen}"'$(prompt_pwd)$(battery_pct_prompt)'
 
     for plugin in "${plugins[@]}"; do
-        [[ "$plugin" == "git" ]] && PS1=${PS1}'$(git_prompt_info)' && BASH_THEME_GIT_PROMPT_DIRTY="\033${FGred} *\033${RCLR}$(git_prompt_status)";
+        [[ "$plugin" == "git" ]] && PS1=${PS1}'$(git_prompt_info)' && BASH_THEME_GIT_PROMPT_DIRTY="${FGred} *${RCLR}$(git_prompt_status)";
         [[ "$plugin" == "svn" ]] && PS1=${PS1}'$(svn_prompt_info)';
     done
 
-    PS1=${PS1}'$(active_screens_prompt)$(bg_jobs_prompt)$(st_jobs_prompt)'"${PR_CLR} >\033${RCLR} \n> "
+    PS1=${PS1}'$(active_screens_prompt)$(bg_jobs_prompt)$(st_jobs_prompt)'"${PR_CLR} >${RCLR} \n> "
     PROMPT_COMMAND=${PROMPT_COMMAND}'PS1=${PS1}'
 
     # PS2 continuation prompt
-    PS2="${PR_CLR} >>\033${RCLR} "
+    PS2="${PR_CLR} >>${RCLR} "
 }
 
 function prompt_off() {
-    PROMPT_COMMAND='PS1="${PR_CLR}${HOSTNAME}\033${FGgreen}$(prompt_pwd)$(active_screens_prompt)\
-$(bg_jobs_prompt)$(st_jobs_prompt)${PR_CLR} >\033${RCLR} \n> "'
+    PROMPT_COMMAND='PS1="${PR_CLR}${HOSTNAME}${FGgreen}$(prompt_pwd)$(active_screens_prompt)\
+$(bg_jobs_prompt)$(st_jobs_prompt)${PR_CLR} >${RCLR} \n> "'
 }
 
 prompt_on
