0fae0ea1f708f333a87f57b0fc71c649dd9a71d5
Lucas Petherbridge <lukexor@gmail.com>
2021-12-09 09:34:06 -0800

    Updated install

--- install.nu
+++ install.nu
@@ -1,9 +1,66 @@
 #!/usr/bin/env nu
 
-let brew_packages = [azure-cli bash cc65 cmake coreutils docker fzf git gnutls gradle helm hexedit kotlin ktlint kubernetes-cli llvm neovim node openjdk openjdk@11 openssl postgresql prettier python python3 sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_ttf sqlite stow tmux tree tree-sitter vim watchman wget yarn]
+let brew_packages = [
+  azure-cli
+  bash
+  cc65
+  cmake
+  coreutils
+  docker
+  fzf
+  git
+  gnutls
+  gradle
+  helm
+  hexedit
+  kotlin
+  ktlint
+  kubernetes-cli
+  llvm
+  lolcat
+  lua
+  mongocli
+  neovim
+  node
+  openjdk
+  openjdk@11
+  openssl
+  postgresql
+  prettier
+  python
+  python3
+  sdl2
+  sdl2_gfx
+  sdl2_image
+  sdl2_mixer
+  sdl2_ttf
+  sqlite
+  starship
+  stow
+  tmux
+  tree
+  tree-sitter
+  vim
+  watchman
+  wget
+]
 let npm_packages = [eslint_d]
-let cargo_packages = [ripgrep cargo-asm cargo-expand cargo-count flamegraph cargo-generate cargo-outdated cargo-readme cargo-tree cargo-watch wasm-pack]
-let cargo_components = [clippy-preview rust-analysis rust-src]
+let cargo_packages = [
+  cargo-asm
+  cargo-count
+  cargo-expand
+  cargo-flamegraph
+  cargo-generate
+  cargo-outdated
+  cargo-readme
+  cargo-tree
+  cargo-watch
+  flamegraph
+  neovide
+  ripgrep
+  wasm-pack
+]
+let cargo_components = [clippy rust-analysis]
 
 echo $brew_packages | each { brew install $it }
 
--- setup.sh
+++ setup.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
+#--------------------------#
+# Install/Update packages #
+#-------------------------#
+
+case "$OSTYPE" in
+  linux*)
+    if command -v apt &>/dev/null; then
+      apt update
+      apt install stow pkg-config libssl-dev libxcb-composite0-dev libx11-dev
+    else if  command -v yum &>/dev/null; then
+      yum update
+      yum install stow libxcb openssl-devel libX11-devel
+    else
+      echo "unsupported package manager"
+      exit 1
+    fi
+    ;;
+  darwin*)
+    xcode-select --install
+    if command -v brew &>/dev/null; then
+        brew update
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install stow openssl cmake
+    ;;
+  *)
+    echo "unsupported: $OSTYPE"
+    exit 1
+    ;;
+esac
+
 #--------------#
 # Install Rust #
 #--------------#
 
-if command -v rustup >/dev/null 2>&1; then
+if command -v rustup &> /dev/null; then
     rustup update
 else
-    curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 #-----------------#
 # Install nushell #
 #-----------------#
 
-cargo install nu --features=extra,table-pager
-
-#--------------#
-# Install Stow #
-#--------------#
-
-##-------##
-## Linux ##
-##-------##
-
-# TODO
-
-##-------##
-## macOS ##
-##-------##
-
-xcode-select --install
-
-if command -v brew >/dev/null 2>&1; then
-    brew update
-else
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
-fi
-
-brew install stow
+cargo install nu --features=extra
 
 #-----------------#
 # Symlink Configs #
