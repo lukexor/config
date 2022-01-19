3e7d1d6fb75994b9728989c8bca4f4d11c645058
Lucas Petherbridge <lukexor@gmail.com>
2022-01-18 16:39:35 -0800

    Fixed relative file path

--- files/.config/nvim/lua/lualine-setup.lua
+++ files/.config/nvim/lua/lualine-setup.lua
@@ -10,7 +10,7 @@ require('lualine').setup{
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'SleuthIndicator', 'ObsessionStatus', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
