38c2eaef97d8188f0fbb912e3804fa45f1dafd4c
Lucas Petherbridge <lukexor@gmail.com>
2022-11-13 10:40:47 -0800

    fixes

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -749,7 +749,7 @@ Plug("nvim-lualine/lualine.nvim", {
     require("lualine").setup {
       options = {
         icons_enabled = true,
-        theme = "gruvbox",
+        theme = "ayu",
         component_separators = { left = " ", right = " " },
         section_separators = { left = " ", right = " " },
         disabled_filetypes = {},
