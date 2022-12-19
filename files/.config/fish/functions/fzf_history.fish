ba095514f8d1fda5e1a91e58a84a15da91795e1a
Lucas Petherbridge <lukexor@gmail.com>
2022-12-19 07:35:07 -0800

    updates

--- files/.config/fish/functions/fzf_history.fish
+++ files/.config/fish/functions/fzf_history.fish
@@ -1,4 +1,4 @@
 function fzf_history -d "fuzzy search history to execute"
-    commandline -r (builtin history | fzf)
+    commandline -r (builtin history | fzf +s --history-size=200)
     commandline -f repaint
 end
