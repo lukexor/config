f4ce5a39fd553704a88a5282663c5cb4d37c8c5d
Lucas Petherbridge <lukexor@gmail.com>
2023-02-02 07:45:57 -0800

    fix: updated local config

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -31,7 +31,31 @@ end
 # Settings   {{{1
 # =============================================================================
 
-fish_config theme choose "ayu Dark"
+set -U fish_color_normal B3B1AD
+set -U fish_color_command 39BAE6
+set -U fish_color_quote C2D94C
+set -U fish_color_redirection FFEE99
+set -U fish_color_end F29668
+set -U fish_color_error FF3333
+set -U fish_color_param B3B1AD
+set -U fish_color_comment 626A73
+set -U fish_color_match F07178
+set -U fish_color_selection --background=E6B450
+set -U fish_color_search_match --background=E6B450
+set -U fish_color_history_current --bold
+set -U fish_color_operator E6B450
+set -U fish_color_escape 95E6CB
+set -U fish_color_cwd 59C2FF
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 4D5566
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
 set fish_color_valid_path
 set fish_pager_color_prefix 'normal' '--bold'
 
@@ -95,15 +119,13 @@ if test -e $agent_info
     set -gx SSH_AGENT_PID (rg -o '=\d+' $agent_info | string replace '=' '')
 end
 
-# TODO: virtualenv with .env
-if test -d ~/preveil
-    set -gx ARCADEROOT ~/preveil/ARCADE/arcade_2.5/
-    set -gx RUST_LOG debug,lapin=off,hyper=off,reqwest::connect=off
-end
-
 function fish_title
     set -q argv[1]; or set argv fish
     echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
+end
+
+if test -e ~/.local/config.fish
+    source ~/.local/config.fish
 end
 
 # =============================================================================
