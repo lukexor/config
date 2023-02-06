a6ee605da70694c218f9d776ed8ab1b45cb97b43
Lucas Petherbridge <lukexor@gmail.com>
2023-02-06 11:59:44 -0800

    fix quotes

--- files/.config/nvim/snippets/all.lua
+++ files/.config/nvim/snippets/all.lua
@@ -1,7 +1,7 @@
 function Uuid()
   local random = math.random
   math.randomseed(os.time())
-  local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  local template = '"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"'
   return (string.gsub(template, '[xy]', function(c)
     local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
     return string.format('%x', v)
