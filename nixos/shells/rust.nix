301f3c4dc65ad2297042bf99a85cac6a2dd6969d
Lucas Petherbridge <luke.petherbridge@statheros.tech>
2024-08-20 19:24:23 -0700

    fix rust shell

--- nixos/shells/rust.nix
+++ nixos/shells/rust.nix
@@ -1,9 +1,15 @@
 with (import <nixpkgs> {
   overlays = [
-    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"));
+    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
   ];
 }); mkShell {
   buildInputs = [
+    # (rust-bin.stable.latest.default.override {
+    #   extensions = [
+    #     "rust-analyzer"
+    #     "rust-src" # for rust-analyzer
+    #   ];
+    # })
     (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
       extensions = [
         "rust-analyzer"
