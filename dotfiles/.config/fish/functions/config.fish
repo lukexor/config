1d2c8a9b9262999c60d64111866467a69e429e80
Luke <lukexor@gmail.com>
2024-10-06 13:47:33 -0700

    updated bindings

--- dotfiles/.config/fish/functions/config.fish
+++ dotfiles/.config/fish/functions/config.fish
@@ -2,17 +2,17 @@ function config -a config -d "edit a configuration file"
     set -l config $argv[1]
     switch $config
         case nvim
-            nvim ~/config/.config/nvim/init.lua
+            nvim ~/.config/nvim/init.lua
         case kitty
-            nvim ~/config/.config/kitty/kitty.conf
+            nvim ~/.config/kitty/kitty.conf
         case fish
-            nvim ~/config/.config/fish/config.fish
+            nvim ~/.config/fish/config.fish
         case fishl
             nvim ~/.local/config.fish
         case nix
             nixos-rebuild.sh
         case starship
-            nvim ~/config/.config/starship.toml
+            nvim ~/.config/starship.toml
         case ""
             echo "Error! must pass a config to edit"
             return 1
